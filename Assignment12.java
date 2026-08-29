8fcd6073075c610fd7c8c9fb1167abc8d1f83ce4
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-29 10:37:58 +0530

    assignment 12

--- Assignment12.java
+++ Assignment12.java
@@ -1,9 +1,17 @@
 public class Assignment12{
     public static void main(String[] args) {
-        Holiday h1 = new Holiday("Iday",15,"August");
-        Holiday h2 = new Holiday("RakshaBdhan", 28, "August");
-        Holiday h3 = new Holiday("Rday",26,"January");
+        Holiday h1 = new Holiday("Iday",6,"August");
+        Holiday h2 = new Holiday("RakshaBdhan", 5, "August");
+        Holiday h3 = new Holiday("Rday",4,"January");
         System.out.println(h1.issameMonth(h3));
+        Holiday [] hd = new Holiday[5];
+        hd[0] = h1;
+        hd[1] = h2;
+        hd[2] = h3;
+        hd[3] = new Holiday("js",4,"sept");
+        hd[4] = new Holiday("js",4,"sept");
+        double avg = h1.avg(hd);
+        System.out.println(avg);
     }
 }
 
@@ -21,5 +29,12 @@ class Holiday{
             return true;
         }
         return false;
+    }
+    public double avg(Holiday [] hd){
+        int sum = 0 ;
+        for(int i = 0 ; i < hd.length ; i++){
+            sum += hd[i].day;
+        }
+        return (double)sum/hd.length;
     }
 }
